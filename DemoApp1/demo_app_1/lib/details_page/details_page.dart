2238fe062b05925e9779fba83a647edde4e9b823
Michał Reszka <64952841+reszka-m@users.noreply.github.com>
2021-06-29 21:30:45 +0200

    2

---
+++ DemoApp1/demo_app_1/lib/details_page/details_page.dart
@@ -0,0 +1,11 @@
+import 'package:demo_app_1/details_page/body.dart';
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Body();
+  }
+}
